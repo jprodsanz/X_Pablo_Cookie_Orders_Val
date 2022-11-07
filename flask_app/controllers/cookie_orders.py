430d6ff7e927e7610f8e449fde18777121d2095a
Pablo X <jprodsanz@gmail.com>
2022-11-07 05:47:09 -0600

    hello_world_1

---
+++ flask_app/controllers/cookie_orders.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, session, redirect, request, url_for
+from flask_app import app
+from flask_app.models.cookie_order import Cookie_order
+
+@app.route("/")
+@app.route("/cookies")
+def log():
+    orders = Cookie_order.get_all()
+    return render_template("orders.html", orders=orders)
+
+@app.route("/cookies/new")
+def new_page():
+    
+    return render_template("log.html")
+
+@app.route("/cookies/edit/<int:cookie_id>")
+def edit_page(cookie_id):
+    order = Cookie_order.get_by_id(cookie_id)
+
+    return render_template("edit_orders.html", order = order)
+
+@app.route("/cookies", methods=["POST"])
+def create_cookie():
+    cookie_order = request.form
+
+    if not Cookie_order.is_valid(cookie_order):
+        return redirect("/cookies/new")
+
+    Cookie_order.create(cookie_order)
+    
+    return redirect("/")
+
+@app.route("/cookies/edit/<int:cookie_id>", methods=["POST"])
+def update_cookie(cookie_id):
+    cookie_order = request.form
+
+    if not Cookie_order.is_valid(cookie_order):
+        return redirect(f"/cookies/edit/{cookie_id}")
+
+    Cookie_order.update(cookie_order)
+    
+    return redirect("/")
